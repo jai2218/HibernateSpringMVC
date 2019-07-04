ad09fe661078e9a392fbb08f26f9161b614a3399
Jai Shanker <jsp.grd@gmail.com>
2019-07-04 16:02:36 +0530

    refs #3
    UI is added and all resources files added.

--- SpringMVCProject/src/main/java/com/jai/SpringMVCProject/HomeController.java
+++ SpringMVCProject/src/main/java/com/jai/SpringMVCProject/HomeController.java
@@ -16,24 +16,54 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+
+		model.addAttribute("serverTime", formattedDate);
+
+		return "index";
 	}
-	
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String indexhome(Model model) {
+		model.addAttribute("command");
+		return "index";
+	}
+
+	@RequestMapping(value = "/form_component", method = RequestMethod.GET)
+	public String formcomponent(Model model) {
+		model.addAttribute("command");
+		return "form_component";
+	}
+
+	@RequestMapping(value = "/profile", method = RequestMethod.GET)
+	public String formprofile(Model model) {
+		model.addAttribute("command");
+		return "profile";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String formlogin(Model model) {
+		model.addAttribute("command");
+		return "login";
+	}
+
+	@RequestMapping(value = "/registration", method = RequestMethod.GET)
+	public String register(Model model) {
+		model.addAttribute("command");
+		return "registration";
+	}
+
 }
